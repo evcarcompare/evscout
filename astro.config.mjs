317a691f5190fae4068a2c721cec330ee0d779be
Nikil Rao <nirao@redhat.com>
2026-04-23 19:18:21 +0100

    Update site URL to new org name evcarcompare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ownwebsitecreate.github.io',
+  site: 'https://evcarcompare.github.io',
   base: '/evscout',
 });
